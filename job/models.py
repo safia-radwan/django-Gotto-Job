9e25b83f02933fc4308a321bd3005bbc14b722c4
safia <safiaradwan950@gmail.com>
2025-08-04 03:53:14 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+JOB_TYPE=(
+    ('Intenship','Intenship'),
+    ('Freelance','Freelance')
+)
+class job(models.Model):
+    title =models.CharField(max_length=100)
+    past_at=models.DateTimeField(auto_now=True)
+    salary=models.IntegerField(default=0)
+    job_type=models.CharField(max_length=15,choices=JOB_TYPE)
+    decription=models.TextField(max_length=1000,default="")
+    about_campany=models.TextField(max_length=1000,default="")
+    experience=models.IntegerField(default=1)
